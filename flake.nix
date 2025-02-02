f5b14d599448fe229a27c41df45b30b7b4daee1d
Corin (CWStra) <cwstra@gmail.com>
2025-02-02 17:35:17 -0500

    Tweaking

--- flake.nix
+++ flake.nix
@@ -19,6 +19,13 @@
       devShells = forAllSystems ({pkgs}: 
       let
         selected-dotnet-sdk = pkgs.dotnet-sdk_9;
+        ld_pkgs = with pkgs; [
+          SDL2
+          SDL2_ttf
+          SDL2_mixer
+          SDL2_image
+        ];
+        ld_path = nixpkgs.lib.strings.concatStringsSep ":" (map (pkg: pkg + "/lib") ld_pkgs);
       in 
       {
         default = pkgs.mkShell {
@@ -26,7 +33,7 @@
             selected-dotnet-sdk
             steam-run
             jetbrains.rider
-          ];
+          ] ++ ld_pkgs;
           shellHook = 
             if selected-dotnet-sdk.version < "9.0.0" 
             then
@@ -36,6 +43,7 @@
             else 
               ''
                 export MSBuildSDKsPath=${selected-dotnet-sdk}/share/dotnet/sdk/${selected-dotnet-sdk.version}/Sdks
+                export LD_LIBRARY_PATH=${ld_path}:/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH
               '';
         };
       });
--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
 let
   pkgs = import<nixpkgs> {};
+  selected-dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0_1xx;
 in
   pkgs.mkShell rec {
     name = "dotnet-env";
     packages = with pkgs; [
-      dotnetCorePackages.sdk_8_0_1xx
+      selected-dotnet-sdk
       steam-run
       jetbrains.rider
+      mono
     ];
+    shellHook = ''
+      export MSBuildSDKsPath=${selected-dotnet-sdk}/share/dotnet/sdk/${selected-dotnet-sdk.version}/Sdks
+    '';
   }
